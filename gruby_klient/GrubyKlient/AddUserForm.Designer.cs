9f7c71e95e48d1e5fb33ba20d86a65ccf3d285f4
Jakub Rajski <czeskij@gmail.com>
2015-04-12 19:25:41 +0200

    interfejs okna dodawania uzytkownika

---
+++ gruby_klient/GrubyKlient/AddUserForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace GrubyKlient
+{
+    partial class AddUserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxPermissionsLevel = new System.Windows.Forms.ComboBox();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxSecondName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxPasswordVerify = new System.Windows.Forms.TextBox();
+            this.labelPermissionsLevel = new System.Windows.Forms.Label();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelSecondName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelPasswordVerify = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxPermissionsLevel
+            // 
+            this.comboBoxPermissionsLevel.FormattingEnabled = true;
+            this.comboBoxPermissionsLevel.Location = new System.Drawing.Point(156, 25);
+            this.comboBoxPermissionsLevel.Name = "comboBoxPermissionsLevel";
+            this.comboBoxPermissionsLevel.Size = new System.Drawing.Size(190, 21);
+            this.comboBoxPermissionsLevel.TabIndex = 0;
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(156, 52);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(190, 20);
+            this.textBoxFirstName.TabIndex = 1;
+            // 
+            // textBoxSecondName
+            // 
+            this.textBoxSecondName.Location = new System.Drawing.Point(156, 78);
+            this.textBoxSecondName.Name = "textBoxSecondName";
+            this.textBoxSecondName.Size = new System.Drawing.Size(190, 20);
+            this.textBoxSecondName.TabIndex = 2;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(156, 104);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(190, 20);
+            this.textBoxLastName.TabIndex = 3;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(156, 130);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(190, 20);
+            this.textBoxEmail.TabIndex = 4;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(156, 156);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(190, 20);
+            this.textBoxPassword.TabIndex = 5;
+            // 
+            // textBoxPasswordVerify
+            // 
+            this.textBoxPasswordVerify.Location = new System.Drawing.Point(156, 182);
+            this.textBoxPasswordVerify.Name = "textBoxPasswordVerify";
+            this.textBoxPasswordVerify.Size = new System.Drawing.Size(190, 20);
+            this.textBoxPasswordVerify.TabIndex = 6;
+            // 
+            // labelPermissionsLevel
+            // 
+            this.labelPermissionsLevel.AutoSize = true;
+            this.labelPermissionsLevel.Location = new System.Drawing.Point(29, 28);
+            this.labelPermissionsLevel.Name = "labelPermissionsLevel";
+            this.labelPermissionsLevel.Size = new System.Drawing.Size(87, 13);
+            this.labelPermissionsLevel.TabIndex = 7;
+            this.labelPermissionsLevel.Text = "Permissions level";
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(29, 55);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(55, 13);
+            this.labelFirstName.TabIndex = 7;
+            this.labelFirstName.Text = "First name";
+            // 
+            // labelSecondName
+            // 
+            this.labelSecondName.AutoSize = true;
+            this.labelSecondName.Location = new System.Drawing.Point(29, 81);
+            this.labelSecondName.Name = "labelSecondName";
+            this.labelSecondName.Size = new System.Drawing.Size(73, 13);
+            this.labelSecondName.TabIndex = 7;
+            this.labelSecondName.Text = "Second name";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(29, 107);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(56, 13);
+            this.labelLastName.TabIndex = 7;
+            this.labelLastName.Text = "Last name";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(29, 133);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(35, 13);
+            this.labelEmail.TabIndex = 7;
+            this.labelEmail.Text = "E-mail";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(29, 159);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(53, 13);
+            this.labelPassword.TabIndex = 7;
+            this.labelPassword.Text = "Password";
+            // 
+            // labelPasswordVerify
+            // 
+            this.labelPasswordVerify.AutoSize = true;
+            this.labelPasswordVerify.Location = new System.Drawing.Point(29, 185);
+            this.labelPasswordVerify.Name = "labelPasswordVerify";
+            this.labelPasswordVerify.Size = new System.Drawing.Size(81, 13);
+            this.labelPasswordVerify.TabIndex = 7;
+            this.labelPasswordVerify.Text = "Verify password";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(271, 244);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(32, 244);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 9;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // AddUserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 291);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelPasswordVerify);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelSecondName);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.labelPermissionsLevel);
+            this.Controls.Add(this.textBoxPasswordVerify);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxSecondName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.comboBoxPermissionsLevel);
+            this.Name = "AddUserForm";
+            this.Text = "Add New User";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPermissionsLevel;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxSecondName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxPasswordVerify;
+        private System.Windows.Forms.Label labelPermissionsLevel;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelSecondName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelPasswordVerify;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ gruby_klient/GrubyKlient/AddUserForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrubyKlient
+{
+    public partial class AddUserForm : Form
+    {
+        public AddUserForm()
+        {
+            CenterToParent();
+            InitializeComponent();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
